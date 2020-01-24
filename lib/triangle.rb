1f28234358b598a790fa32e7b2aef01154f54c01
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-24 02:28:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  
+  def initialize (l1,l2,l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+  
+  def kind (equilateral:, isosceles:, scalene:)
+    if @l1 == @l2 && @l2 == @l3
+      self.kind = :equilateral
+      elsif @l1 == @l2 || @l2 == @l3
+      self.kind = :isosceles
+    else 
+      self.kind = :scalene
+  
+  end
+  
+  
+  class TriangleError < StandardError
+  
+  end
+  
+  
 end
